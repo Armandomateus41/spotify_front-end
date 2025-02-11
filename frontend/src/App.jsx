8fe4f02e6c98573de79db34f6c1cea8419fe9c0e
Armandomateus41 <armandomateus41@gmail.com>
2025-02-11 17:08:50 -0300

    atualizar 11-02-2025

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
+import Header from "./components/Header";
+import Artist from "./pages/Artists/Artist";
+import Artists from "./pages/Artists/Artists";
+import Song from "./pages/Songs/Song";
+import Songs from "./pages/Songs/Songs";
+import Home from "./pages/Home/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  console.log("App component");
-
   return (
-    <div>
-      <h1>Ola Mundo!</h1>
-      <p>Projeto</p>
-    </div>
-  )
+    <>
+      <Header />
+
+    
+        <Route path="/" element={<Home />} />
+        <Route path="/artist/:id" element={<Artist />} />
+        <Route path="/artists" element={<Artists />} />
+        <Route path="/song/:id" element={<Song />} />
+        <Route path="/songs" element={<Songs />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
